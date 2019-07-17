ae4574902f6ca4e0760955e11379ae8b92b5c751
Brian Amesbury <bamesbury@sofi.org>
2019-07-17 13:49:37 -0600

    initial commit

---
+++ amessage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  amessage
+//
+//  Created by Brian Amesbury on 7/17/19.
+//  Copyright © 2019 Moonlit Door. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
